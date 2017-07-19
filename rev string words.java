807f9f32630ebb5fb56eb528324129687759fd35
keerthanajanani <keerthijanani66@gmail.com>
2017-07-19 18:00:41 +0530

    Create rev string words.java

---
+++ rev string words.java
@@ -0,0 +1,17 @@
+# rev-string-words
+import java.io.*;
+import java.util.*;
+public class rev string words
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter the input string:");
+String s1=br.readLine();
+String[] ch=s.split(" ");
+for(int i=ch.length-1;i>=0;i--)
+{
+System.out.println(ch[i]+" ");
+}
+}
+}
